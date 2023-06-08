0eaeb69f8c7d0c6a26fb6be6044077b65eebfc08
estefany2006 <136000202+estefany2006@users.noreply.github.com>
2023-06-08 10:08:12 -0600

    Initial commit

---
+++ database/migrations/2023_06_06_162820_create_inventories_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\Municipality;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inventories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Book::class)->constrained();
+            $table->string('address');
+            $table->foreignIdFor(Municipality::class)->constrained();
+            $table->float('price');
+            $table->integer('image');
+            $table->boolean('available');
+            $table->string('description');
+
+            $table->foreignIdFor(User::class)->constrained();
+            $table->float('fee');
+            $table->date('date');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inventories');
+    }
+};
