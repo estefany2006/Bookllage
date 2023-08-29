1cf8262c82a67b6643276b0486f8f3b08f7957b0
dianagnomez <136000298+dianagnomez@users.noreply.github.com>
2023-08-29 09:59:03 -0600

    registerbook

--- resources/views/bookTransaction.blade.php
+++ resources/views/bookTransaction.blade.php
@@ -1,37 +1,50 @@
-
 <x-layout>
 
-    <div class="container bg-success p-2 text-dark bg-opacity-50 position-absolute top-50 start-50 translate-middle">
-        <form action="/signup" method="POST">
-            @csrf
+    <style>
+        .form-box input:focus {
+            box-shadow: 0 0 0;
+            border: 2px solid #766BBF;
+        }
+    </style>
 
-            <x-input label="Image" Type="file" name="image" />
+    <section class="main">
+        <div class="container my-5">
+            <div class="row">
+                <div class="col-lg-7 col-md-12 "></div>
+                <div class="col-lg-5 col-md-10 col-sm-12"></div>
+                <div class="form-box px-5 py-4">
+                    <div class="p-3 mb-2 bg-primary-subtle text-emphasis-primary">
+                        <h2 class="text-center mb-4">Register Book</h2>
+                        <form action="/signup" method="POST">
+                            @csrf
 
-            <x-input label="Address" Type="string" name="address" />
+                            <x-input label="Image" Type="file" name="image" />
 
-            <x-input label="Price" Type="number" step="0.01" name="price" />
+                            <x-input label="Address" Type="string" name="address" />
 
-            <x-input label="Descripcion" Type="string" name="descripcion" />
+                            <x-input label="Price" Type="number" step="0.01" name="price" />
 
-            <div class="form-check">
-                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" checked>
-                <label class="form-check-label" for="flexRadioDefault1">
-                  Available
-                </label>
-              </div>
-              <div class="form-check">
-                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2">
-                <label class="form-check-label" for="flexRadioDefault2">
-                  Unavailable
-                </label>
-              </div>
+                            <x-input label="Descripcion" Type="string" name="descripcion" />
 
-              <x-buttom type="submit" text="upload" />
-        </form>
-    </div>
+                            <div class="form-check">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                <label class="form-check-label" for="exampleCheck1">available</label>
+                            </div>
+                            <div class="form-check">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                <label class="form-check-label" for="exampleCheck1">unavailable</label>
+                            </div>
+                            <div class="mt-2">
+                                <x-buttom type="submit" text="upload" />
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
 
 
 </x-layout>
-
-
--- resources/views/registerBook.blade.php
+++ resources/views/registerBook.blade.php
@@ -1,21 +1,44 @@
 <x-layout>
 
-    <div class="container bg-success p-2 text-dark bg-opacity-50 position-absolute top-50 start-50 translate-middle">
-        <form action="registerBook" method="POST">
-            @csrf
+<style>
 
-            <x-input label="Book name" Type="text" name="name" />
+     .form-box input:focus {
+        box-shadow: 0 0 0;
+        border:2px solid #766BBF;
+     }
 
-            <x-input label="Publisher" Type="text" name="publisher" />
+</style>
 
-            <x-input label="Edition" Type="text" name="edition" />
+    <section class="main">
+        <div class="container my-5">
+            <div class="row">
+                <div class="col-lg-7 col-md-12 "></div>
+                <div class="col-lg-5 col-md-10 col-sm-12"></div>
+                <div class="form-box px-5 py-4">
+                    <div class="p-3 mb-2 bg-primary-subtle text-emphasis-primary">
 
-            <x-input label="Date" Type="date" name="date" />
+                        <h2 class="text-center mb-4">Register Book</h2>
+                        <form action="registerBook" method="POST">
+                            @csrf
+                            <x-input label="Book name" Type="text" name="name" class="form-control"/>
 
-            <x-buttom type="submit" text="upload" />
+                            <x-input label="Publisher" Type="text" name="publisher" class="form-control"/>
 
-        </form>
-    </div>
+                            <x-input label="Edition" Type="text" name="edition" />
+
+                            <x-input label="Date" Type="date" name="date"/>
+
+                            <div class="mb-3 form-check">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                <label class="form-check-label" for="exampleCheck1">Information confirmation</label>
+                            </div>
+                            <x-buttom type="submit" text="Next"/>
+                        </form>
+                    </div>
+                </div>
+              </div>
+        </div>
+    </section>
 
     @if (session('success'))
         <div class="alert alert-success">
