ad82d03c93e0c4d4629af0ffe0b654f780f4f45d
Anibal-M <136000692+Anibal-M@users.noreply.github.com>
2023-09-16 09:15:52 -0600

    style

--- resources/views/bookTransaction.blade.php
+++ resources/views/bookTransaction.blade.php
@@ -1,8 +1,19 @@
 <x-layout>
     <style>
+        body {
+            background-image: url("img/Libros.jpg");
+            background-size: cover;
+            background-repeat: no-repeat;
+        }
+
         .form-box input:focus {
             box-shadow: 0 0 0;
             border: 2px solid #766BBF;
+        }
+
+        .register {
+            /*background: linear-gradient(#5688a7, transparent);*/
+            background: linear-gradient(#284259af, #8e81e662, #a6b2f777);
         }
     </style>
 
@@ -16,9 +27,9 @@
 
                 <div class="col-9">
                     <div class="form-box px-5 py-4">
-                        <div class="p-3 mb-2 bg-primary-subtle text-emphasis-primary">
-                            <h5 class="text-center mb-4">Add the transaction details</h5>
-                            <p>and publish your book</p>
+                        <div class="register p-3 mb-2">
+                            <h5 class="text-center mb-4" style="color: #FFFFFF;">Add the transaction details</h5>
+                            <p style="color: #FFFFFF;">and publish your book</p>
                             <form action="/signup" method="POST">
                                 @csrf
 
@@ -31,12 +42,16 @@
                                     @endforeach
                                 </select>
 
+                                <br>
+
                                 <select class="form-select" aria-label="Default select example" name="Address">
                                     <option selected>Municipality</option>
                                     @foreach ($municipalities as $municipality)
                                         <option value="{{ $municipality->id }}">{{ $municipality->name }}</option>
                                     @endforeach
                                 </select>
+
+                                <br>
 
                                 <select class="form-select" aria-label="Default select example" name="Address">
                                     <option selected>District</option>
@@ -45,7 +60,15 @@
                                     @endforeach
                                 </select>
 
+                                <br>
+
                                 <x-input label="Price" Type="number" step="0.01" name="price" />
+
+                                <div class="input-group mb-3" Type="number" name="price">
+                                    <span class="input-group-text">$</span>
+                                    <span class="input-group-text">0.00</span>
+                                    <input type="number" class="form-control" aria-label="Dollar amount (with dot and two decimal places)">
+                                  </div>
 
                                 <x-input label="Descripcion" Type="string" name="descripcion" />
 
--- resources/views/registerBook.blade.php
+++ resources/views/registerBook.blade.php
@@ -1,19 +1,30 @@
 <x-layout>
     <x-nav.navbar/>
     <style>
+        body {
+            background-image: url("img/Libros.jpg");
+            background-size: cover;
+            background-repeat: no-repeat;
+        }
+
         .form-box input:focus {
             box-shadow: 0 0 0;
             border: 2px solid #766BBF;
         }
+
+        .register{
+        /*background: linear-gradient(#5688a7, transparent);*/
+        background: linear-gradient(#284259af, #8e81e662, #a6b2f777);
+    }
     </style>
 
     <section class="main" style="margin-top: 80px">
         <div class="container my-5">
             <div class="row">
                 <div class="form-box px-5 py-4">
-                    <div class="p-3 mb-2 bg-primary-subtle text-emphasis-primary">
+                    <div class="register p-3 mb-2 ">
 
-                        <h2 class="text-center mb-4">Make sure that the book is already registered</h2>
+                        <h2 class="text-center mb-4" style="color: #FFFFFF;">Make sure that the book is already registered</h2>
                         <form action="selectBook" method="POST">
                             @csrf
                             <select class="form-select" aria-label="Default select example" name="book">
@@ -28,9 +39,9 @@
                     </div>
                 </div>
                 <div class="form-box px-5 py-4">
-                    <div class="p-3 mb-2 bg-primary-subtle text-emphasis-primary">
+                    <div class="register p-3 mb-2 ">
 
-                        <h2 class="text-center mb-4">Register</h2>
+                        <h2 class="text-center mb-4" style="color: #FFFFFF;">Register</h2>
                         <form action="registerBook" method="POST">
                             @csrf
                             <x-input label="Book name" Type="text" name="name" class="form-control" />
