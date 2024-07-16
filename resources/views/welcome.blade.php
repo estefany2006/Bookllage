53c42faa12b4d9c8729b10d6e40ae263354e13f4
estefany2006 <136000202+estefany2006@users.noreply.github.com>
2024-07-16 10:23:06 -0600

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,6 +24,7 @@
             }
         }
 
+
         @media (max-width: 768px) {
 
             #intro,
@@ -135,8 +136,8 @@
 
     <div class="container">
         <div class="row mx-auto my-5 align-items-center">
-            <div class="col-lg-7 col-md-8 col-sm-12" >
-                <p style="text-align: justify; font-size: 120%; font-family:'Teachers'" >
+            <div class="col-lg-7 col-md-8 col-sm-12">
+                <p style="text-align: justify; font-size: 120%; font-family:'Teachers'">
                     BOOKET is a platform to find university books in a simple and fast way.
                     We are a team committed to education and believe in the importance of
                     learning. Imagine a space where you can get advantages and access to a
@@ -165,17 +166,20 @@
                         need another opportunity to serve the college community.
                     </p>
                 </div>
+                <div class="col-4 col-md-4">
+                    <img class="img-fluid rounded" src="{{ asset('img/Nosotros.jpg') }}" alt="nosotros1">
+                </div>
 
-                <div class="container" id="cardContainer">
+                <div class="container" id="tracking-in-expand">
                     <div class="row row-cols-6 g-4 justify-content-center mt-5">
 
                         <div class="col-12 col-md-3">
-                            <img src="{{ asset('img/Tatiana.jpg') }}" class="card-img-top d-flex justify-content-center"
+                            <img src="{{ asset('img/Estefany.jpg') }}" class="card-img-top d-flex justify-content-center"
                                 alt="Estefany" style="}">
 
-                            <div class="card-body mt-3 text-center" style="font-family:'Teachers'" >
+                            <div class="card-body mt-3 text-center" style="font-family:'Teachers'">
                                 <h5 class="card-title">Estefany Mancia</h5>
-                                <p class="card-text">Marketing area.</p>
+                                <p class="card-text">Marketing area</p>
                             </div>
 
                         </div>
@@ -201,16 +205,40 @@
                             <img src="{{ asset('img/Anibal.jpg') }}" class="card-img-top d-flex justify-content-center"
                                 alt="Estefany">
                             <div class="card-body mt-3 text-center" style="font-family:'Teachers'">
-                                <h5 class="card-title" >Anibal Mendez</h5>
+                                <h5 class="card-title">Anibal Mendez</h5>
                                 <p class="card-text">Back-end developer.</p>
                             </div>
                         </div>
                         <div class="col-12 col-md-3">
-                            <img src="{{ asset('img/Yamileth.png') }}"
+                            <img src="{{ asset('img/Yami.jpg') }}"
                                 class="card-img-top d-flex justify-content-center" alt="Estefany">
                             <div class="card-body mt-3 text-center">
                                 <h5 class="card-title" style="font-family:'Teachers'">Ana Garcia</h5>
                                 <p class="card-text" style="font-family:'Teachers'">Front-end developer.</p>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-3">
+                            <img src="{{ asset('img/Nelson.jpg') }}"
+                                class="card-img-top d-flex justify-content-center" alt="Estefany">
+                            <div class="card-body mt-3 text-center">
+                                <h5 class="card-title" style="font-family:'Teachers'">Nelson Sanchez</h5>
+                                <p class="card-text" style="font-family:'Teachers'">Front-end developer.</p>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-3">
+                            <img src="{{ asset('img/Pamela.jpg') }}"
+                                class="card-img-top d-flex justify-content-center" alt="Estefany">
+                            <div class="card-body mt-3 text-center">
+                                <h5 class="card-title" style="font-family:'Teachers'">Pamela Lainez</h5>
+                                <p class="card-text" style="font-family:'Teachers'">Marketing area.</p>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-3">
+                            <img src="{{ asset('img/Carlos.jpg') }}"
+                                class="card-img-top d-flex justify-content-center" alt="Estefany">
+                            <div class="card-body mt-3 text-center">
+                                <h5 class="card-title" style="font-family:'Teachers'">Carlos Chacon</h5>
+                                <p class="card-text" style="font-family:'Teachers'">Back-end developer.</p>
                             </div>
                         </div>
                     </div>
@@ -229,7 +257,8 @@
             <img src="{{ asset('img/mission.jpg') }}" width="180c" height="160">
             <div>
                 <h3 class="fs-5 mt-4 px-4 pb-1" style="font-family:'Teachers'">Mission</h3>
-                <p class="px-4" style="font-family:'Teachers'">Streamline the process of finding college books for students' higher
+                <p class="px-4" style="font-family:'Teachers'">Streamline the process of finding college books for
+                    students' higher
                     education, encouraging the reuse of college book establishing a secure and reliable
                     connection between college students, with the goal of reducing the financial burden on
                     college students at the same time. </p>
@@ -239,7 +268,8 @@
             <img src="{{ asset('img/vision.jpg') }}" width="180" height="160">
             <div>
                 <h3 class="fs-5 mt-4 px-4 pb-1" style="font-family:'Teachers'">Vision</h3>
-                <p class="px-4" style="font-family:'Teachers'">Our vision is to reach the entire university community and be the first
+                <p class="px-4" style="font-family:'Teachers'">Our vision is to reach the entire university community
+                    and be the first
                     choice for university students seeking academic resources, becoming a platform
                     recognized for the innovation and accessibility that "BOOKET" offers. We aspire to
                     positively impact the lives of college students by facilitating their access to the
@@ -248,13 +278,13 @@
         </div>
         <hr class="my-5">
         <div class="col-12 my-5">
-            <h5 class="title" style="font-family: 'Roboto Serif', serif">Our values</h5>
+            <h5 class="title" style="font-family:'Teachers">Our values</h5>
         </div>
         <div class="col-lg-4 col-md-6 col-sm-12 mt-3">
             <img src="{{ asset('img/inovation.jpg') }}" alt="Inovation" width="180c" height="160"></a>
             <div>
                 <h3 class="fs-5 mt-4 px-4 pb-1" style="font-family:'Teachers'">Innovation</h3>
-                <p class="px-4">we are promoting a solution using technology, with the purpose of
+                <p class="px-4" style="font-family:'Teachers'; fornt-size:'100%'">We are promoting a solution using technology, with the purpose of
                     effectively addressing the current challenges often faced by university students.</p>
             </div>
         </div>
@@ -262,7 +292,8 @@
             <img src="{{ asset('img/motivation.jpg') }}" alt="Motivation" width="180" height="160"></a>
             <div>
                 <h3 class="fs-5 mt-4 px-4 pb-1" style="font-family:'Teachers'">Motivation</h3>
-                <p class="px-4" style="font-family:'Teachers'">We aspire to incentivize to students to push them limits and reduce the
+                <p class="px-4" style="font-family:'Teachers'">We aspire to incentivize to students to push them
+                    limits and reduce the
                     incidence of university dropouts due to economic constraints.</p>
             </div>
         </div>
@@ -270,7 +301,8 @@
             <img src="{{ asset('img/overcoming.jpg') }}" alt="Ovecomming" width="180c" height="160"></a>
             <div>
                 <h3 class="fs-5 mt-4 px-4 pb-1" style="font-family:'Teachers'">Overcoming</h3>
-                <p class="px-4" style="font-family:'Teachers'">We wish to contribute to the field of higher education through the promotion
+                <p class="px-4" style="font-family:'Teachers'">We wish to contribute to the field of higher
+                    education through the promotion
                     of motivation.</p>
             </div>
         </div>
@@ -278,7 +310,8 @@
             <img src="{{ asset('img/Quality.jpg') }}" alt="Quality" width="180" height="160"></a>
             <div>
                 <h3 class="fs-5 mt-4 px-4 pb-1" style="font-family:'Teachers'">Quality</h3>
-                <p class="px-4" style="font-family:'Teachers'">We are committed to offer products in good condition and high-quality service
+                <p class="px-4" style="font-family:'Teachers'">We are committed to offer products in good condition
+                    and high-quality service
                     on our platform.</p>
             </div>
         </div>
@@ -286,36 +319,38 @@
             <img src="{{ asset('img/Security.jpg') }}" alt="Security" width="180c" height="160"></a>
             <div>
                 <h3 class="fs-5 mt-4 px-4 pb-1" style="font-family:'Teachers'">Security and confidence</h3>
-                <p class="px-4" style="font-family:'Teachers'">Our goal is to ensure the integrity of user data and build trust in our
+                <p class="px-4" style="font-family:'Teachers'">Our goal is to ensure the integrity of user data and
+                    build trust in our
                     platform.</p>
             </div>
         </div>
 
     </div>
 
-    <div id="our_work" style="scroll-margin-top: 150px">
-        <hr class="my-5">
-        <h2 class="text-center my-5 title" style="font-family:'Teachers'">Our work</h2>
+    <div class="container">
+        <div id="our_work" style="scroll-margin-top: 150px">
+            <hr class="my-5">
+            <h2 class="text-center my-5 title" style="font-family:'Teachers'">Our work</h2>
 
-        <div class="row mx-auto my-5 align-items-center">
-            <div class="col-lg-7 col-md-7 col-sm-12">
-                <p style="font-size: 120%; text-align:justify font-family:'Teachers">
-                    At BOOKET, our passion is education and accessibility
-                    to knowledge. We are proud to offer an online platform
-                    that simplifies the search for college books, providing
-                    students and lovers of learning a space where they can
-                    access a wide variety of academic book categories. But
-                    our work goes beyond being a search platform; we are
-                    agents of change in the educational community. Every book
-                    you find on BOOKET represents a second chance to inspire
-                    others and expand the horizon of knowledge. We are committed
-                    to fostering equal educational opportunities and contributing to
-                    the empowerment of students everywhere.
-                </p>
+            <div class="row mx-auto my-5 align-items-center">
+                <div class="col-lg-12 col-md-8 col-sm-12">
+                    <p style="font-size: 120%; text-align:justify; font-family:'Teachers'">
+                        At BOOKET, our passion is education and accessibility
+                        to knowledge. We are proud to offer an online platform
+                        that simplifies the search for college books, providing
+                        students and lovers of learning a space where they can
+                        access a wide variety of academic book categories. But
+                        our work goes beyond being a search platform; we are
+                        agents of change in the educational community. Every book
+                        you find on BOOKET represents a second chance to inspire
+                        others and expand the horizon of knowledge. We are committed
+                        to fostering equal educational opportunities and contributing to
+                        the empowerment of students everywhere.
+                    </p>
+                </div>
             </div>
         </div>
     </div>
-
 
     <footer class="py-5 bg-dark">
         <div class="container">
